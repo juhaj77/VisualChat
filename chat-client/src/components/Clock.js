617ed6b2ca384d03475e85f07d2dae5064577501
juhaj77 <juhaj358@gmail.com>
2023-10-24 02:03:05 +0300

    memory leak fix

--- chat-client/src/components/Clock.js
+++ chat-client/src/components/Clock.js
@@ -1,15 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import { connect } from 'react-redux'
 
 
 const Clock = (props) => {
 	
   const [time, setTime] = useState('')
-  let timeoutId = -1
+  const timeoutId = useRef(0)
 
   useEffect(() => {
     updateTime(props.user)
     return () => {
+      clearTimeout(timeoutId.current)
       updateTime(undefined)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -18,10 +19,10 @@ const Clock = (props) => {
   const updateTime = (user) => {
     const date = new Date()
     //jostain syystä tämä date onkin jo Suomiajassa
-    clearTimeout(timeoutId)
+    clearTimeout(timeoutId.current)
     if(user){
       setTime(date.toString().slice(16,24))
-      timeoutId=setTimeout(updateTime.bind(null,user),1000)
+      timeoutId.current=setTimeout(updateTime.bind(null,user),1000)
     }
   }
 
